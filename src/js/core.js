60d6f4cfe52fa74f66aa778d9c871a9d71b02666
Taxiozaurus <taxiozaurus@gmail.com>
2018-03-12 21:32:07 +0000

    Dismissable alerts

---
+++ src/js/core.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function(){
+	var body = document.querySelector('body');
+	body.addEventListener('click', function(event) {
+		if(event.target.classList.contains('dismiss-btn')) {
+			_dismissElement(event.target);
+		}
+	});
+
+	function _dismissElement(elem) {
+		elem.parentNode.classList.add('dismiss');
+		setTimeout(function() {
+			elem.parentNode.remove();
+		}, 2000);
+	}
+}, false);
