3093abe40177822786fecf11465bdec3a4306cfc
Taxiozaurus <taxiozaurus@gmail.com>
2018-03-13 21:28:20 +0000

    Update dismiss js

--- src/js/core.js
+++ src/js/core.js
@@ -1,15 +1,20 @@
 document.addEventListener('DOMContentLoaded', function(){
 	var body = document.querySelector('body');
 	body.addEventListener('click', function(event) {
-		if(event.target.classList.contains('dismiss-btn')) {
+		if(event.target.hasAttribute('dismiss')) {
 			_dismissElement(event.target);
 		}
 	});
 
 	function _dismissElement(elem) {
-		elem.parentNode.classList.add('dismiss');
+		var root = elem.closest('.alert');
+		if(!root) {
+			return false;
+		}
+		root.classList.add('dismiss');
 		setTimeout(function() {
-			elem.parentNode.remove();
-		}, 1000);
+			root.remove();
+		}, 700);
+		return true;
 	}
 }, false);
