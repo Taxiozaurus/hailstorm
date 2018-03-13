12a38863db276153f277c8616c37c3281d448207
Taxiozaurus <taxiozaurus@gmail.com>
2018-03-13 21:56:52 +0000

    Moving things again

--- src/js/core.js
+++ src/js/core.js
@@ -1,11 +1,30 @@
-document.addEventListener('DOMContentLoaded', function(){
-	var body = document.querySelector('body');
-	body.addEventListener('click', function(event) {
-		if(event.target.hasAttribute('dismiss')) {
-			_dismissElement(event.target);
-		}
-	});
+/**
+ * Hailstorm JS
+ *
+ * @author Alexey "Taxiozaurus" Makeev <taxiozaurus@gmail.com>
+ */
+window.Hailstorm = new function() {
+	/**
+	 * Container for 'body' element
+	 */
+	var body;
 
+	/**
+	 * Init all the listeners
+	 */
+	function _init() {
+		body = document.querySelector('body');
+		body.addEventListener('click', function(event) {
+			if(event.target.hasAttribute('dismiss')) {
+				_dismissElement(event.target);
+			}
+		});
+	}
+
+	/**
+	 * Run dismissal animation and remove the element
+	 * @param {Element} elem
+	 */
 	function _dismissElement(elem) {
 		var root = elem.closest('.alert');
 		if(!root) {
@@ -17,4 +36,15 @@ document.addEventListener('DOMContentLoaded', function(){
 		}, 700);
 		return true;
 	}
+
+	this.init = function() {
+		_init()
+	};
+};
+
+/**
+ * Start up the Hailstorm
+ */
+document.addEventListener('DOMContentLoaded', function(){
+	Hailstorm.init();
 }, false);
