dc3e0768adcac5c045a0025e2a0e24693fb811d1
Taxiozaurus <taxiozaurus@gmail.com>
2018-10-11 11:59:15 +0000

    working fancy forms

--- src/js/core.js
+++ src/js/core.js
@@ -13,11 +13,30 @@ window.Hailstorm = new function() {
 	 * Init all the listeners
 	 */
 	function _init() {
+		// grab document body
 		body = document.querySelector('body');
+		
+		// Add dismiss event listeners, 
+		// adding listener to body allows adding dismissable elements 
+		// without rebinding this listener
 		body.addEventListener('click', function(event) {
 			if(event.target.hasAttribute('dismiss')) {
 				_dismissElement(event.target);
 			}
+		});
+
+		// Update fancy form elements to have appropriate wrappers
+		var inputs = document.querySelectorAll('.form-input.form-fancy:not(.ready)');
+		var inputWrapper = document.createElement('div');
+		inputWrapper.classList.add('form-input-wrapper');
+		var inputBar = document.createElement('div');
+		inputBar.classList.add('form-fancy-input-bar');
+		Array.prototype.forEach.call(inputs, function(elem) {
+			var wrapperClone = inputWrapper.cloneNode();
+			elem.parentNode.insertBefore(wrapperClone, elem);
+			wrapperClone.appendChild(elem);
+			wrapperClone.appendChild(inputBar.cloneNode());
+			elem.classList.add('ready');
 		});
 	}
 
